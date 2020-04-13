e60101243b8c324875abee03b6dcade234eaa8a9
Elikio <elikiopaots@gmail.com>
2020-04-13 16:49:03 +0800

    commit

---
+++ client/build/precache-manifest.f141cbaf40c872953f5110fbe97d930d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec33f0e460c1ba023807c1516ce4b334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d799b11ae115750b02b",
+    "url": "/static/css/main.52eb4ac8.chunk.css"
+  },
+  {
+    "revision": "40b318ed7023c2ce4aba",
+    "url": "/static/js/2.96df75b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96df75b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d799b11ae115750b02b",
+    "url": "/static/js/main.eb1757f3.chunk.js"
+  },
+  {
+    "revision": "75b8a4d58fec9727727a",
+    "url": "/static/js/runtime-main.3e04baf7.js"
+  }
+]);
