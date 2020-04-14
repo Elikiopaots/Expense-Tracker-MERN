73e665d675c3c9c036e1f01fc1ca3f02cb6d475e
Elikio <elikiopaots@gmail.com>
2020-04-14 12:31:18 +0800

    responsive design 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {GlobalProvider} from './component/GlobalState'
+import { GlobalProvider } from './component/GlobalState';
 import Header from './component/Header';
 import Balance from './component/Balance';
 import IncExp from './component/IncExp';
@@ -9,13 +9,13 @@ import AddTxn from './component/AddTxn';
 
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
       <GlobalProvider>
-      <Header />
-      <Balance />
-      <IncExp />
-      <TxnList />
-      <AddTxn />
+        <Header />
+        <Balance />
+        <IncExp />
+        <TxnList />
+        <AddTxn />
       </GlobalProvider>
     </div>
   );
